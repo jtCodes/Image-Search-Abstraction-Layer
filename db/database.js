925d8e26572f5b5728d246405665dcf4ac6d1be0
jt <jt234wq@gmail.com>
2017-09-05 23:07:26 -0400

    database info change

--- db/database.js
+++ db/database.js
@@ -2,12 +2,13 @@ const { Pool } = require('pg')
 const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
-    /*
+  
     connectionString: connectionString,
-    */
+    /*
     host: 'localhost',
     database: 'image_search',
     port: 5432,
+    */
 })
 pool.connect()
 
